39391e91e85700557dc52eb5c7b114bb507ff8d2
Luke <luke.e.philips@gmail.com>
2017-03-07 10:51:44 -0800

    home#index only shows uncategorized runs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,27 +1,27 @@
 class HomeController < ApplicationController
   def index
+    @uncategorized_runs ||= current_user.runs.where(goal_id: nil)
     if current_user.token
       @client = Strava::Api::V3::Client.new(:access_token => current_user.token)
       @athlete ||= @client.retrieve_current_athlete
-      @activities ||= @client.list_athlete_activities
       @users = User.all
 
-      if current_user.runs.empty?
-        @activities.first(20).each do |activity|
-          current_user.runs.create(start_latlng: activity['start_latlng'], end_latlng: activity['end_latlng'], total_distance: activity['distance'], total_time: activity['elapsed_time'], travel_method: activity['type'], strava_id: activity['id'], date: Time.parse(activity['start_date_local']))
-          puts "RUNS CREATED"
-        end
-      else
-        # up_to_date = false
-        # while up_to_date == false
-          @activities.first(20).each do |activity|
-            if !current_user.runs.where(strava_id: activity['id']).exists?
-              current_user.runs.create(start_latlng: activity['start_latlng'], end_latlng: activity['end_latlng'], total_distance: activity['distance'], total_time: activity['elapsed_time'], travel_method: activity['type'], strava_id: activity['id'], date: Time.parse(activity['start_date_local']))
-              puts "RUN #{activity['id']} CREATED"
-            end
-          # end
-        end
-      end
+      # if current_user.runs.empty?
+      #   @activities.first(20).each do |activity|
+      #     current_user.runs.create(start_latlng: activity['start_latlng'], end_latlng: activity['end_latlng'], total_distance: activity['distance'], total_time: activity['elapsed_time'], travel_method: activity['type'], strava_id: activity['id'], date: Time.parse(activity['start_date_local']))
+      #     puts "RUNS CREATED"
+      #   end
+      # else
+      #   # up_to_date = false
+      #   # while up_to_date == false
+      #     # @activities.first(20).each do |activity|
+      #     #   if !current_user.runs.where(strava_id: activity['id']).exists?
+      #     #     current_user.runs.create(start_latlng: activity['start_latlng'], end_latlng: activity['end_latlng'], total_distance: activity['distance'], total_time: activity['elapsed_time'], travel_method: activity['type'], strava_id: activity['id'], date: Time.parse(activity['start_date_local']))
+      #     #     puts "RUN #{activity['id']} CREATED"
+      #     #   end
+      #     # end
+      #   # end
+      # end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,29 +9,47 @@ class User < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name
 
-  attr_accessor :client, :athlete, :activities
+  # attr_accessor :client, :athlete, :activities
 
-  def client
-    @client
-    # @client = Strava::Api::V3::Client.new(:access_token => ENV['ACCESS_TOKEN'])
-
-    # access_information = Strava::Api::V3::Auth.retrieve_access(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], 'code')
-  end
-  def athlete
-    @athlete
-    # @athlete ||= self.client.retrieve_current_athlete
-  end
-  def activities
-    @activities
-    # @activities ||= self.client.list_athlete_activities
-  end
-
-  # after_initialize do
-  #   if user_signed_in? && self.runs.empty?
-  #     self.activities.first(10).each do |activity|
-  #       self.runs.create(start_latlng: activity['start_latlng'], end_latlng: activity['end_latlng'], total_distance: activity['distance'], total_time: activity['elapsed_time'], travel_method: activity['type'], strava_id: activity['id'])
-  #       puts "RUNS CREATED"
-  #     end
-  #   end
+  # def client
+  #   @client
+  #   # @client = Strava::Api::V3::Client.new(:access_token => ENV['ACCESS_TOKEN'])
+  #
+  #   # access_information = Strava::Api::V3::Auth.retrieve_access(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], 'code')
   # end
+  # def athlete
+  #   @athlete
+  #   # @athlete ||= self.client.retrieve_current_athlete
+  # end
+  # def activities
+  #   @activities
+  #   # @activities ||= self.client.list_athlete_activities
+  # end
+
+  def after_database_authentication
+    puts "DB call to populate runs"
+    if self.token
+      @client = Strava::Api::V3::Client.new(:access_token => self.token)
+      @athlete ||= @client.retrieve_current_athlete
+      @activities ||= @client.list_athlete_activities
+      @users = User.all
+
+      if self.runs.empty?
+        @activities.first(20).each do |activity|
+          self.runs.create(start_latlng: activity['start_latlng'], end_latlng: activity['end_latlng'], total_distance: activity['distance'], total_time: activity['elapsed_time'], travel_method: activity['type'], strava_id: activity['id'], date: Time.parse(activity['start_date_local']))
+          puts "RUNS CREATED"
+        end
+      else
+        # up_to_date = false
+        # while up_to_date == false
+          @activities.first(20).each do |activity|
+            if !self.runs.where(strava_id: activity['id']).exists?
+              self.runs.create(start_latlng: activity['start_latlng'], end_latlng: activity['end_latlng'], total_distance: activity['distance'], total_time: activity['elapsed_time'], travel_method: activity['type'], strava_id: activity['id'], date: Time.parse(activity['start_date_local']))
+              puts "RUN #{activity['id']} CREATED"
+            end
+          # end
+        end
+      end
+    end
+  end
 end
