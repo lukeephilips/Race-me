749c30e4c6026415713215b3f73bfa63688e836a
Luke <luke.e.philips@gmail.com>
2017-03-07 14:13:28 -0800

    edit migration ChangeRunsTotalTime from add_column to change_column

--- db/migrate/20170228020612_change_runs_total_time.rb
+++ db/migrate/20170228020612_change_runs_total_time.rb
@@ -1,5 +1,5 @@
 class ChangeRunsTotalTime < ActiveRecord::Migration[5.0]
   def change
-    add_column :runs, :total_time, :integer, default: 0
+    change_column :runs, :total_time, :integer, default: 0
   end
 end
