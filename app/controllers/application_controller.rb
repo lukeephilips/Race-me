d4f72bea95f23a1d55f2016f6636b35cf948d83a
Luke <luke.e.philips@gmail.com>
2017-03-08 09:39:47 -0800

    authentication is still weird, but its at least pretty seamless within the construct of unintegrated Strava and Devise. It will have to do for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,17 @@ class ApplicationController < ActionController::Base
     access_token = access_information['access_token']
     current_user.update({token: access_token})
 
+    byebug
+    if current_user.token && current_user.runs.empty?
+      @client = Strava::Api::V3::Client.new(:access_token => current_user.token)
+      @activities ||= @client.list_athlete_activities
+
+      @activities.first(20).each do |activity|
+        current_user.runs.create(start_latlng: activity['start_latlng'], end_latlng: activity['end_latlng'], total_distance: activity['distance'], total_time: activity['elapsed_time'], travel_method: activity['type'], strava_id: activity['id'], date: Time.parse(activity['start_date_local']))
+        puts "RUNS CREATED"
+      end
+    end
+
     redirect_to root_path
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if current_user.runs.any?
       @user = current_user
-    @runs ||= current_user.runs.where(goal_id: nil)
+      @runs ||= current_user.runs.where(goal_id: nil)
     end
 
     if current_user.token
