d80351cac258a26201456826db1e7bf5f01e3b47
Luke <luke.e.philips@gmail.com>
2017-05-11 08:53:28 -0700

    and a bunch of other shit

---
+++ db/migrate/20170422231731_create_pictures.rb
@@ -0,0 +1,11 @@
+class CreatePictures < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pictures do |t|
+      t.string :title
+      t.string :body
+      t.attachment :image
+
+      t.timestamps
+    end
+  end
+end
