aaeb117ecfbf00dca2bcc5c2777faddf4e4d71cb
Luke <luke.e.philips@gmail.com>
2017-05-19 08:17:06 -0700

    uh oh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:token_exchange]
 
-  def current_user
-    @current_user ||= super && User.includes(:goals, :runs, :races).find(@current_user.id)
-  end
-
   def token_exchange
     code = params['code']
     access_information =  Strava::Api::V3::Auth.retrieve_access(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], code)
@@ -15,7 +11,6 @@ class ApplicationController < ActionController::Base
       token: access_token})
 
     current_user.check_for_token_and_runs
-    flash[:notice] = "Nice work! Create a goal and assign your runs"
     redirect_to root_path
   end
 end
