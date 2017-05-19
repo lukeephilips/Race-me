a3b2bc50b2b18322489a56c83c6f6b28e8533aee
Luke <luke.e.philips@gmail.com>
2017-05-18 21:15:55 -0700

    fix bounce thing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:token_exchange]
 
   def current_user
     @current_user ||= super && User.includes(:goals, :runs, :races).find(@current_user.id)
